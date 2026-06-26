afa36f5ed9d38fdfaf608ad10d47abc0b071e0ce
Monika <56404498+monika314@users.noreply.github.com>
2026-06-26 13:51:46 +0200

    Update home.js

--- fnaftitlegen/js/home.js
+++ fnaftitlegen/js/home.js
@@ -1,31 +1,31 @@
 var data;
+// un-needed stuff
+// function bgChange(x)
+// {
+//     var imgs = ['images/431.png', 'images/440.png', 'images/441.png', 'images/442.png'];
+//     document.getElementById("freddy").src = imgs[x];
+// }
 
-function bgChange(x)
-{
-    var imgs = ['images/431.png', 'images/440.png', 'images/441.png', 'images/442.png'];
-    document.getElementById("freddy").src = imgs[x];
-}
-
-function bgRand()
-{
-    var x=Math.floor(Math.random() * 100)
-    switch (x)
-    {
-        case 99:
-            bgChange(3);
-            break;
-        case 98:
-            bgChange(2);
-            break;
-        case 97:
-            bgChange(1);
-            break;
-    }
-    if (x < 97)
-    {
-        bgChange(0);
-    }
-}
+// function bgRand()
+// {
+//     var x=Math.floor(Math.random() * 100)
+//     switch (x)
+//     {
+//         case 99:
+//             bgChange(3);
+//             break;
+//         case 98:
+//             bgChange(2);
+//             break;
+//         case 97:
+//             bgChange(1);
+//             break;
+//     }
+//     if (x < 97)
+//     {
+//         bgChange(0);
+//     }
+// }
   
 function genTitle()
 {
@@ -33,7 +33,7 @@ function genTitle()
     var s = grammar.flatten("#origin#");
     if(s=="((origin))")
     {
-        document.getElementById("gen").innerHTML = "Five Nights at Fetch Error";
+        document.getElementById("gen").innerHTML = "Five Nights at Error, fix it bitch.";
     }else{
         document.getElementById("gen").innerHTML = s;
     }
