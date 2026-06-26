dec0c00120d9edeffff9955089ff17776570c447
Monika <56404498+monika314@users.noreply.github.com>
2026-06-26 13:49:48 +0200

    Update titles.js

--- fnaftitlegen/js/titles.js
+++ fnaftitlegen/js/titles.js
@@ -6,7 +6,7 @@ import data from '/fnaftitlegen/titles.json' assert {type: 'json'};
 */
 
 async function returnTitles(){
-	return await fetch("https://raw.githubusercontent.com/happydeijean/fnaftitlegenmonika/main/titles.json")
+	return await fetch("https://raw.githubusercontent.com/monika314/fnaftitlegenmonika/main/titles.json")
 	.then(response => response.json())
 	.then(data => {
 		return data;
